743d6d8af50d61afa388014540596f2d7e8df006
luyuil <19057235886@163.com>
2026-09-22 22:35:45 +0800

    学习了多分类问题，有点没搞懂，先上传一部分代码

---
+++ Pytorch_study/multi-class classification problem.py
@@ -0,0 +1,30 @@
+import torch
+from torchvision import transforms
+from torchvision import datasets
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torch.optim as optim
+
+batch_size = 64
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.1307, ),(0.3081, ))
+])
+
+train_dataset = datasets.MNIST(root='../dataset/mnist/',
+                               train=True,
+                               download=True,
+                               transform=transform)
+
+train_loader = DataLoader(train_dataset,
+                          shuffle=True,
+                          batch_size=batch_size)
+
+test_dataset = datasets.MNIST(root='../dataset/mnist/',
+                              train=False,
+                              download=True,
+                              transform=transform)
+
+test_loader = DataLoader(test_dataset,
+                         shuffle=False,
+                         batch_size=batch_size)
